c818058a69c3b041da6e24366b1e13f196525611
tisasunny <63533370+tisasunny@users.noreply.github.com>
2020-10-20 10:41:31 +0530

    Add files via upload

---
+++ freq.java
@@ -0,0 +1,14 @@
+public class freq
+{
+	public static void main(String[] args) {
+	    String str="Wish you a good day";
+		char c='o';
+		int freq=0;
+		System.out.println("Tisa Sunny");
+		System.out.println("62");
+		for(int i=0;i<str.length();i++)
+		if(c==str.charAt(i))
+		freq++;
+	   System.out.println("Frequency of "+c+" in the string '"+str+"' is "+freq);
+	}
+}
