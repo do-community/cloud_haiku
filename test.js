ec788600729bbce915dd7aea1acd31892f3e469a
Apple <apple@Apples-MacBook-Air.local>
2024-10-12 22:00:06 +0530

    Some chnages in test.js file and package.json

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 require('tap').mochaGlobals()
 const assert = require('assert')
-const remark = require('remark')
+import remark from 'remark';
 const frontmatter = require('remark-frontmatter')
 const extract = require('remark-extract-frontmatter')
 const yaml = require('yaml').parse
